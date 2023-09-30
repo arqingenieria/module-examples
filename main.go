b6971cca5fa9daef70927a4abc034a4caa129b99
Oscar José Antonio Pinto Salazar <oscar.pinto@vidacamara.cl>
2023-09-30 00:16:20 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		list := []string{"EDteam", "gophers", "golang", quote.HelloV3()}
+
+		slices.Filter(list, func(item string) bool {
+			return strings.HasPrefix(strings.ToLower(item), "h")
+		})
+
+		fmt.Println(quote.Concurrency())
+	*/
+
+	fmt.Println("inicio")
+}
